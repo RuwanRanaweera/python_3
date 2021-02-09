61a2377963a5c8e6765e0f2f474d09ac1032bc4e
ruwan <rwnranaweera@gmail.com>
2021-02-09 15:50:40 +0530

    no commit

---
+++ 6.py
@@ -0,0 +1,5 @@
+for letter in 'python':
+    if letter == 'h':
+        pass
+    print('This is pass block')
+    
---
+++ 7.py
@@ -0,0 +1,7 @@
+count = 1
+while count < 11:
+    if count%2==0:
+        pass
+    else:
+        print count
+    count = count + 1
